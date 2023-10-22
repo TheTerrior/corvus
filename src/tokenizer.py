f05578bfe4249d006dc6b63665e6ca53168d04ce
TheTerrior <werbird10@gmail.com>
2023-10-22 04:48:40 -0700

    Finished improved tokenization implementation

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -148,13 +148,47 @@ def tokenize_new(input_file: str) -> tools.Result[list[tuple[int, int, list[str]
     global symbols
 
     all_tokens: list[tuple[int, int, list[str]]] = [] #holds all tokens, [line_number, indentation, tokens]
-    line_number = 0
+    line_number = 1
     in_comment = False #inside of a multiline comment
+    multiline_start = 0 #starting index of multiline comment
 
     with open(input_file, "r") as file:
         while (line := file.readline()) != "": #iterate through every line
             tokens: list[str] = []
             token: str = ""
+
+            # remove comments
+            while True:
+                if in_comment: #currently inside a multiline
+                    if loc := line.find("$$") != -1: #found end to multiline comment
+                        line = line[loc+2:] #remove multiline comment
+                        in_comment = False
+                    else: #no end to multiline
+                        break
+
+                loc_m = line.find("??")
+                loc_s = line.find("//")
+                if loc_s != -1 or loc_m != -1: #found some comment, ordered purposefully to exit condition sooner
+                    if loc_s != -1 and (loc_m == -1 or loc_s < loc_m): #remove single-line comment
+                        line = line[:loc_s]
+                    #elif loc_m != -1 and (loc_s == -1 or loc_m < loc_s): #remove multiline comment
+                    else: #remove multiline comment
+                        sub = line[loc_m+2:] #get the end of line substring
+                        loc_u = sub.find("$$") #check for a multiline end
+                        if loc_u != -1: #end of multiline found
+                            line = line[:loc_m] + line[loc_u+2:]
+                        else: #multiline is multi-line
+                            in_comment = True
+                            multiline_start = line_number
+                            line = line[:loc_m]
+                            break #no more comments after this
+
+                else: #no comments found
+                    break
+
+            if len(line) == 0: #whole line remove by comments
+                line_number += 1
+                continue
 
             # count indentation
             counter = 0 #how many spaces there are
@@ -165,64 +199,34 @@ def tokenize_new(input_file: str) -> tools.Result[list[tuple[int, int, list[str]
                     break
             if counter % 4 != 0: #invalid number of indents
                 return tools.err(f"invalid indentation on line {line_number}")
-            indentation = counter / 4
+            indentation = counter // 4
             line = line.strip() #remove indentation
-
-            # remove comments
-            #loc = line.find("//")
-            #if loc != -1: #single-line comment found
-            #    line = line[:loc].strip() #remove comment
-            #for c in line:
-            #    loc_m = line.find("$$")
-            #    loc_s = line.find("//")
-            #    if loc != -1: #found one half of multiline comment
-
-            # remove comments better
-            while True:
-                if in_comment and (loc := line.find("??")) != -1: #found end to multiline comment
-                    line = line[loc+2:] #remove multiline comment
-                    in_comment = False
-                loc_m = line.find("??")
-                loc_s = line.find("//")
-                if loc_s != -1 or loc_m != -1: #found some comment, ordered purposefully to exit condition sooner
-                    if loc_m != 1 and loc_s != -1: #both exist
-                        if loc_s < loc_m: #single comment first
-                            line = line[:loc_s] #remove single-line comment
-                        else: #multi comment first
-                            sub = line[loc_m+2:] #get the end of line
-                            loc_u = sub.find("$$") #check for a multiline end
-                            if loc_u != -1: #end of multiline found
-                                line = line[:loc_m] + line[loc_u+2:] #remove multiline comment
-                            else: #multiline is multi-line
-                                in_comment = True
-                                line = line[:loc_m] #remove multiline comment
-                    elif loc_s != 1:
-                        line = line[:loc_s] #remove single-line comment
-                    else:
-                        sub = line[loc_m+2:] #get the end of line
-                        loc_u = sub.find("$$") #check for a multiline end
-                        if loc_u != -1: #end of multiline found
-                            line = line[:loc_m] + line[loc_u+2:] #remove multiline comment
-                        else: #multiline is multi-line
-                            in_comment = True
-                            line = line[:loc_m] #remove multiline comment
-                else: #no comments found
-                    break
-
 
             # tokenize line
             for c in line:
-                if c not in symbols: #normal symbol
+                if c == " ":
+                    if token != "":
+                        tokens.append(token)
+                        token = ""
+                    else:
+                        continue
+                elif c not in symbols: #normal symbol
                     token += c
                 else: #reserved symbol
-                    tokens.append(token)
+                    if token != "":
+                        tokens.append(token)
+                        token = ""
                     tokens.append(c)
-                    token = ""
+            if token != "":
+                tokens.append(token)
 
-
+            all_tokens.append((line_number, indentation, tokens))
             line_number += 1
+    
+    if in_comment:
+        return tools.err(f"unclosed multiline comment starting at line {multiline_start}")
 
-
+    return tools.ok(all_tokens)
     
 
 
